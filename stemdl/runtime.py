b27301260b97be347d5c6d26f0044c445103c18d
Somnath, Suhas <somnaths@ornl.gov>
2017-10-11 11:48:57 -0400

    Replace runtime.py

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -7,48 +7,15 @@ email: laanaitn@ornl.gov
 import time
 from datetime import datetime
 import tensorflow as tf
-import re
-from . import network
-from . import inputs
+import network
+import inputs
 
 
-class _LoggerHook(tf.train.SessionRunHook):
-    """Logs loss and runtime stats."""
-    def __init__(self, flags, total_loss, num_gpus):
-        self.flags = flags
-        self.total_loss = total_loss
-        self.num_gpus = num_gpus
-
-    def begin(self):
-        self._step = -1
-        self._start_time = time.time()
-
-    def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs(self.total_loss)  # Asks for loss value.
-
-    def after_run(self, run_context, run_values):
-        if self._step % self.flags.log_frequency == 0:
-            current_time = time.time()
-            duration = current_time - self._start_time
-            self._start_time = current_time
-
-            loss_value = run_values.results
-            examples_per_sec = self.flags.log_frequency * self.flags.batch_size * self.num_gpus / duration
-            sec_per_batch = float(duration / self.flags.log_frequency)
-            elapsed_epochs = self.num_gpus * self._step * self.flags.batch_size / self.flags.NUM_EXAMPLES_PER_EPOCH
-            format_str = ('%s: step = %d, epoch = %2.2e, loss = %.2f (%.1f examples/sec; %.3f '
-                          'sec/batch)')
-            print(format_str % (datetime.now(), self._step, elapsed_epochs, loss_value,
-                                examples_per_sec, sec_per_batch))
-
-
-def _add_loss_summaries(total_loss, losses, flags):
+def _add_loss_summaries(total_loss, losses):
     """
     Add summaries for losses in model.
     Generates moving average for all losses and associated summaries for
     visualizing the performance of the network.
-    :param flags:
     :param total_loss:
     :param losses:
     :return: loss_averages_op
@@ -62,7 +29,6 @@ def _add_loss_summaries(total_loss, losses, flags):
     for l in losses + [total_loss]:
         # Name each loss as '(raw)' and name the moving average version of the loss
         # as the original loss name.
-        loss_name = re.sub('%s_[0-9]*/' % flags.worker_name, '', l.op.name)
         tf.summary.scalar(l.op.name + ' (raw)', l)
         tf.summary.scalar(l.op.name, loss_averages.average(l))
 
@@ -80,9 +46,6 @@ def _average_gradients(worker_grads):
      List of pairs of (gradient, variable) where the gradient has been averaged
      across all workers.
     """
-    if len(worker_grads) == 1:
-        return worker_grads[0]
-
     average_grads = []
     for grad_and_vars in zip(*worker_grads):
         # Note that each grad_and_vars looks like the following:
@@ -129,22 +92,26 @@ def get_optimizer(flags, hyper_params, global_step):
     # Decay the learning rate exponentially based on the number of steps.
     def ramp():
         # lr = INITIAL_LEARNING_RATE*LEARNING_RATE_DECAY_FACTOR**(-global_step/ramp_steps)
-        lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step, ramp_steps, LEARNING_RATE_DECAY_FACTOR,
+        lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                        global_step,
+                                        ramp_steps,
+                                        LEARNING_RATE_DECAY_FACTOR,
                                         staircase=True)
         lr = INITIAL_LEARNING_RATE ** 2 * tf.pow(lr, tf.constant(-1.))
         return tf.cast(lr, tf.float32)
 
     def decay():
-        lr = tf.train.exponential_decay(WARM_UP_LEARNING_RATE_MAX, global_step, decay_steps, LEARNING_RATE_DECAY_FACTOR,
+        lr = tf.train.exponential_decay(WARM_UP_LEARNING_RATE_MAX,
+                                        global_step,
+                                        decay_steps,
+                                        LEARNING_RATE_DECAY_FACTOR,
                                         staircase=True)
         return lr
 
-    if hyper_params['warm_up']:
-        LEARNING_RATE = tf.cond(global_step < ramp_up_steps, ramp, decay)
-        LEARNING_RATE = tf.cond(LEARNING_RATE <= WARM_UP_LEARNING_RATE_MAX, lambda: LEARNING_RATE, decay)
-    else:
-        LEARNING_RATE = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step, decay_steps,
-                                        LEARNING_RATE_DECAY_FACTOR, staircase=True)
+    LEARNING_RATE = tf.cond(global_step < ramp_up_steps, ramp, decay)
+    LEARNING_RATE = tf.cond(LEARNING_RATE <= WARM_UP_LEARNING_RATE_MAX,
+                            lambda: LEARNING_RATE,
+                            decay)
 
     # Summarize learning rate
     tf.summary.scalar('learning_rate', LEARNING_RATE)
@@ -153,7 +120,6 @@ def get_optimizer(flags, hyper_params, global_step):
     if hyper_params['optimization'] == 'SGD':
         opt = tf.train.GradientDescentOptimizer(LEARNING_RATE)
         return opt
-
     # Default is ADAM
     opt = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
     return opt
@@ -210,7 +176,6 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
             for i in range(num_GPUS):
                 with tf.device('/gpu:%d' % i):
                     with tf.name_scope('%s_%d' % (flags.worker_name, i)) as scope:
-                        tf.get_variable_scope().reuse_variables()
 
                         # Setup Neural Net
                         n_net = network.ConvNet(flags, global_step, hyper_params, network_config, images, labels,
@@ -218,23 +183,14 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
                         # Build it
                         n_net.build_model()
 
-                        # calculate the loss
-                        n_net.get_loss()
-
-                        # Assemble all of the losses.
-                        losses = tf.get_collection('losses', scope)
-
-                        # Calculate the total loss for the current worker
-                        total_loss = tf.add_n(losses, name='total_loss')
+                        # calculate the losses
+                        losses, total_loss = n_net.get_loss(scope)
 
                         # Generate summaries for the losses and get corresponding op
-                        loss_averages_op = _add_loss_summaries(total_loss, losses, flags)
+                        loss_averages_op = _add_loss_summaries(total_loss, losses)
 
-                        # Reuse variables for the next worker.
-                        # try:
+                        # Reuse variables for the next tower.
                         tf.get_variable_scope().reuse_variables()
-                        # except ValueError:
-                        #     print('skipping non-shared variables.')
 
                         # get summaries
                         summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
@@ -253,7 +209,7 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
         avg_gradients = _average_gradients(worker_grads)
 
         # Add histograms for gradients.
-        for grad, var in avg_gradients:
+        for grad, var in grads:
             if grad is not None:
                 summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
 
@@ -267,22 +223,46 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
         # Track the moving averages of all trainable variables.
         variable_averages = tf.train.ExponentialMovingAverage(
             hyper_params['moving_average_decay'], global_step)
-        variable_averages_op = variable_averages.apply(tf.trainable_variables())
+        variables_averages_op = variable_averages.apply(tf.trainable_variables())
 
         # Gather all training related ops into a single one.
-        with tf.control_dependencies([apply_gradient_op, variable_averages_op, tf.group(*worker_ops)]):
+        with tf.control_dependencies([apply_gradient_op, variables_averages_op, tf.group(*worker_ops)]):
             train_op = tf.no_op(name='train')
+
+        class _LoggerHook(tf.train.SessionRunHook):
+            """Logs loss and runtime."""
+
+            def begin(self):
+                self._step = -1
+                self._start_time = time.time()
+
+            def before_run(self, run_context):
+                self._step += 1
+                return tf.train.SessionRunArgs(total_loss)  # Asks for loss value.
+
+            def after_run(self, run_context, run_values):
+                if self._step % flags.log_frequency == 0:
+                    current_time = time.time()
+                    duration = current_time - self._start_time
+                    self._start_time = current_time
+
+                    loss_value = run_values.results
+                    examples_per_sec = flags.log_frequency * flags.batch_size * num_GPUS / duration
+                    sec_per_batch = float(duration / flags.log_frequency)
+
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                'sec/batch)')
+                    print (format_str % (datetime.now(), self._step, loss_value,
+                                       examples_per_sec, sec_per_batch))
 
         # Config file for tf.Session()
         config = tf.ConfigProto(allow_soft_placement=flags.allow_soft_placement,
                                 log_device_placement=flags.log_device_placement)
 
-        logHook = _LoggerHook(flags, total_loss, num_GPUS)
-
         # Start Training Session
         with tf.train.MonitoredTrainingSession(
             checkpoint_dir=flags.train_dir,
-            hooks=[tf.train.StopAtStepHook(last_step=flags.max_steps), tf.train.NanTensorHook(total_loss),logHook],
+            hooks=[tf.train.StopAtStepHook(last_step=flags.max_steps), tf.train.NanTensorHook(total_loss),_LoggerHook()],
                 config=config) as mon_sess:
                 while not mon_sess.should_stop():
                     mon_sess.run(train_op)
