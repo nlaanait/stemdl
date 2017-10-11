8b641e7bebdc9cba6f9908fe1e4b94c5fd67f3d2
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-11 11:38:27 -0400

    Catching non-shared variable exceptions.

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -210,45 +210,44 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
             for i in range(num_GPUS):
                 with tf.device('/gpu:%d' % i):
                     with tf.name_scope('%s_%d' % (flags.worker_name, i)) as scope:
+                        tf.get_variable_scope().reuse_variables()
+
+                        # Setup Neural Net
+                        n_net = network.ConvNet(flags, global_step, hyper_params, network_config, images, labels,
+                                             operation='train')
+                        # Build it
+                        n_net.build_model()
+
+                        # calculate the loss
+                        n_net.get_loss()
+
+                        # Assemble all of the losses.
+                        losses = tf.get_collection('losses', scope)
+
+                        # Calculate the total loss for the current worker
+                        total_loss = tf.add_n(losses, name='total_loss')
+
+                        # Generate summaries for the losses and get corresponding op
+                        loss_averages_op = _add_loss_summaries(total_loss, losses, flags)
+
+                        # Reuse variables for the next worker.
                         try:
                             tf.get_variable_scope().reuse_variables()
+                        except ValueError:
+                            print('skipping non-shared variables.')
 
-                            # Setup Neural Net
-                            n_net = network.ConvNet(flags, global_step, hyper_params, network_config, images, labels,
-                                                 operation='train')
-                            # Build it
-                            n_net.build_model()
+                        # get summaries
+                        summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
 
-                            # calculate the loss
-                            n_net.get_loss()
+                        # Calculate the gradients for the current data batch
+                        with tf.control_dependencies([loss_averages_op]):
+                            grads = opt.compute_gradients(total_loss)
 
-                            # Assemble all of the losses.
-                            losses = tf.get_collection('losses', scope)
+                        # Accumulate gradients across all workers.
+                        worker_grads.append(grads)
 
-                            # Calculate the total loss for the current worker
-                            total_loss = tf.add_n(losses, name='total_loss')
-
-                            # Generate summaries for the losses and get corresponding op
-                            loss_averages_op = _add_loss_summaries(total_loss, losses, flags)
-
-                            # Reuse variables for the next worker.
-                            try:
-                                tf.get_variable_scope().reuse_variables()
-                            except ValueError:
-                                print('skipping non-shared variables.')
-
-                            # get summaries
-                            summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
-
-                            # Calculate the gradients for the current data batch
-                            with tf.control_dependencies([loss_averages_op]):
-                                grads = opt.compute_gradients(total_loss)
-
-                            # Accumulate gradients across all workers.
-                            worker_grads.append(grads)
-
-                            # Accumulate extra non-standard operations across workers
-                            worker_ops.append(n_net.get_misc_ops())
+                        # Accumulate extra non-standard operations across workers
+                        worker_ops.append(n_net.get_misc_ops())
 
         # Average gradients over workers.
         avg_gradients = _average_gradients(worker_grads)
