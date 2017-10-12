979a3f53bfb43b013f4b9858fe158c98e4436f34
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-12 07:46:39 -0400

    Catching non-shared variable exceptions.

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -297,7 +297,7 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
                     mon_sess.run(train_op)
                     grad_arr_0, grad_arr_4 = mon_sess.run(worker_grads[0][0])#, worker_grads[4][0][0]])
                     print('worker_0 grads: %s' %format(grad_arr_0))
-                    losses = mon_sess.run(losses)
+                    losses = mon_sess.run(losses[0])
                     print('worker_0 losses: %s' % format(losses))
                     # print('worker_4 grads: %s' % format(grad_arr_4))
 
