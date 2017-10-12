a25eadadd712424f7245acb32f2eb4e41620444f
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-12 16:44:28 -0400

    Catching non-shared variable exceptions.

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -255,8 +255,8 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
         # print(losses)
         # avg_gradients = worker_grads[0]
         # Add histograms for gradients.
-        for grad, var in avg_gradients:
-            summaries.append(tf.summary.histogram(var+ '/gradients', grad))
+        # for grad, var in avg_gradients:
+        #     summaries.append(tf.summary.histogram(var+ '/gradients', grad))
 
         # Apply gradients.
         apply_gradient_op = opt.apply_gradients(avg_gradients, global_step=global_step)
