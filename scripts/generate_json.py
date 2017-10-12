9463f8eeec549c336fa58cae024f32ae5bae7c4f
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-11 22:24:27 -0400

    Catching non-shared variable exceptions.

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -13,23 +13,23 @@ from stemdl import io_utils
 # templates for network_config  #
 #################################
 
-layer_keys_list = ['conv1', 'conv2', 'conv3', 'conv4', 'pool1', 'conv5', 'conv6', 'conv7', 'conv8',
-                   'pool2', 'conv9', 'conv10', 'conv11', 'conv12', 'pool3', 'fc_1', 'linear_output']
+layer_keys_list = ['conv1', 'conv2', 'pool1', 'conv3', 'conv4', 'pool2',
+                   'conv5', 'conv6', 'pool3','fc_1', 'linear_output']
 
 # parameters dictionary
-conv_layer_1 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [4, 4], 'features': 64,
+conv_layer_1 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [8, 8], 'features': 96,
                             'activation':'relu', 'padding':'SAME'})
-conv_layer_2 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [4, 4], 'features': 128,
+conv_layer_2 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [4, 4], 'features': 64,
                             'activation':'relu', 'padding':'SAME'})
 pool_avg = OrderedDict({'type': 'pooling', 'stride': [2, 2], 'kernel': [2, 2], 'pool_type': 'avg','padding':'SAME'})
 pool_max = OrderedDict({'type': 'pooling', 'stride': [2, 2], 'kernel': [2, 2], 'pool_type': 'max','padding':'SAME'})
-conv_layer_3 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [4, 4], 'features': 256,
+conv_layer_3 = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [2, 2], 'features': 64,
                             'activation':'relu', 'padding':'SAME'})
-fully_connected = OrderedDict({'type': 'fully_connected','weights': 1024,'bias': 1024, 'activation': 'relu',
-                               'regularize': True})
+fully_connected = OrderedDict({'type': 'fully_connected','weights': 256,'bias': 256, 'activation': 'relu',
+                               'regularize': False})
 linear_ouput = OrderedDict({'type': 'linear_output','weights': 3,'bias': 3,'regularize': False})
 
-layer_params_list = [conv_layer_1]*4 + [pool_max] + [conv_layer_2]*4 + [pool_max] + [conv_layer_3]*4 + [pool_avg] + \
+layer_params_list = [conv_layer_1]*2 + [pool_avg] + [conv_layer_2]*2 + [pool_avg] + [conv_layer_3]*2 + [pool_avg] + \
                     [fully_connected] + [linear_ouput]
 
 #################################
@@ -42,7 +42,7 @@ hyper_params_regression = {'network_type': 'regressor', #'network_type': 'classi
                            'warm_up': False,
                            'num_epochs_per_decay':30,
                            'learning_rate_decay_factor': 0.5,
-                           'initial_learning_rate': 1.e-3,
+                           'initial_learning_rate': 0.2,
                            'num_epochs_per_ramp': 10,
                            'num_epochs_in_warm_up': 100,
                            'warm_up_max_learning_rate': 1e-3,
