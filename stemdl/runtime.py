77d59b59024aee6dc41bc87ba4a062d4d3af21fd
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-12 22:59:44 -0400

    Adding TODO's.

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -260,9 +260,9 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
         variable_averages_op = variable_averages.apply(tf.trainable_variables())
 
         # Gather all training related ops into a single one.
-        with tf.control_dependencies([apply_gradient_op, variable_averages_op, tf.group(*worker_ops)]):
-            train_op = tf.no_op(name='train')
-        # train_op = tf.group(apply_gradient_op, variable_averages_op)
+        # with tf.control_dependencies([apply_gradient_op, variable_averages_op, tf.group(*worker_ops)]):
+        #     train_op = tf.no_op(name='train')
+        train_op = tf.group(apply_gradient_op, variable_averages_op)
 
         # Config file for tf.Session()
         config = tf.ConfigProto(allow_soft_placement=flags.allow_soft_placement,
