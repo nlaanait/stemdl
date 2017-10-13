06e9c881088e38e9035eb74e38159dac98e5ac45
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-13 11:56:40 -0400

    Working on Tensorboard Summaries.

--- scripts/stemdl_run.py
+++ scripts/stemdl_run.py
@@ -20,8 +20,8 @@ These FLAGS define variables for a particular TF workflow and are not expected t
 tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement.""")
 tf.app.flags.DEFINE_boolean('allow_soft_placement', True, """Whether to allow variable soft placement on the device-""" +\
                      """ This is needed for multi-gpu runs.""")
-tf.app.flags.DEFINE_integer('log_frequency', 100, """How often to log results to the console.""")
-tf.app.flags.DEFINE_integer('save_frequency',100, """How often to save summaries to disk""")
+tf.app.flags.DEFINE_integer('log_frequency', 50, """How often to log results to the console.""")
+tf.app.flags.DEFINE_integer('save_frequency',500, """How often to save summaries to disk""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,"""Number of batches to run.""")
 tf.app.flags.DEFINE_integer('num_epochs', 500,"""Number of Data Epochs to do training""")
 tf.app.flags.DEFINE_integer('NUM_EXAMPLES_PER_EPOCH', 729000,"""Number of examples in training data.""")
