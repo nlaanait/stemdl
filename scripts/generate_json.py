d7674615cf504ceca21add838fa666da0566a273
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-14 11:11:25 -0400

    Starting to work on evaluation methods.

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -32,17 +32,32 @@ linear_ouput = OrderedDict({'type': 'linear_output','weights': 3,'bias': 3,'regu
 layer_params_list = [conv_layer_1]*5 + [pool_avg] + [conv_layer_1]*2 + [conv_layer_2]*3 + [pool_avg] + \
                     [fully_connected] + [linear_ouput]
 
+
+################
+# VGG-16-like #
+##############
+layer_keys_list = ['conv1', 'conv2', 'pool1', 'conv3', 'conv4','pool2', 'conv5','conv6', 'conv7','conv8','pool3', 'conv9', 'conv10','conv11','conv12', 'pool3','conv13', 'conv14', 'conv15', 'conv16','pool4', 'fc_1','fc_2', 'linear_output']
+conv_1 =OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [3, 3], 'features': 64,'activation':'relu', 'padding':'SAME'})
+conv_2 =OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [3, 3], 'features': 128,'activation':'relu', 'padding':'SAME'})
+conv_3 =OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [3, 3], 'features': 256,'activation':'relu', 'padding':'SAME'})
+conv_4 =OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [3, 3], 'features': 512,'activation':'relu', 'padding':'SAME'})
+pool = OrderedDict({'type': 'pooling', 'stride': [2, 2], 'kernel': [2, 2], 'pool_type': 'max','padding':'SAME'})
+fully_connected = OrderedDict({'type': 'fully_connected','weights': 1000,'bias': 1000, 'activation': 'tanh','regularize': True})
+linear_output = OrderedDict({'type': 'linear_output','weights': 3,'bias': 3,'regularize': False})
+layer_params_list = [conv_1]*2 +[pool] + [conv_2]*2 + [pool] + [conv_3]*4 + [pool] + [conv_4]*4 + [pool] + [conv_4]*4 + [pool] + [fully_connected]*2 + [linear_output]
+
+
 #################################
 # templates for hyper-parameters #
 #################################
 
 # Regression
 hyper_params_regression = {'network_type': 'regressor', #'network_type': 'classifier'
-                           'optimization': 'SGD', #'optimization': 'SGD'
+                           'optimization': 'ADAM', #'optimization': 'SGD'
                            'warm_up': False,
                            'num_epochs_per_decay':30,
                            'learning_rate_decay_factor': 0.5,
-                           'initial_learning_rate': 0.01,
+                           'initial_learning_rate': 0.1,
                            'num_epochs_per_ramp': 10,
                            'num_epochs_in_warm_up': 100,
                            'warm_up_max_learning_rate': 1e-3,
@@ -59,8 +74,7 @@ hyper_params_regression = {'network_type': 'regressor', #'network_type': 'classi
 #TODO
 
 if __name__ == '__main__':
-    io_utils.write_json_network_config('network_regressor.json', layer_keys_list, layer_params_list)
-    _ = io_utils.load_json_network_config('network_regressor.json')
-    io_utils.write_json_hyper_params('hyper_params_regressor.json', hyper_params_regression)
-    _ = io_utils.load_json_hyper_params('hyper_params_regressor.json')
-
+    io_utils.write_json_network_config('network_regressor_VGG.json', layer_keys_list, layer_params_list)
+    _ = io_utils.load_json_network_config('network_regressor_VGG.json')
+    io_utils.write_json_hyper_params('hyper_params_regressor_VGG.json', hyper_params_regression)
+    _ = io_utils.load_json_hyper_params('hyper_params_regressor_VGG.json')
