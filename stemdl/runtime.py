363dfaa9b92277b332fb419af74a0a4e645877ea
Laanait, Nouamane <laanaitn@ornl.gov>
2017-10-17 11:52:32 -0400

    Debugging Evaluation Methods.

--- stemdl/runtime.py
+++ stemdl/runtime.py
@@ -290,13 +290,16 @@ def train(network_config, hyper_params, data_path, flags, num_GPUS=1):
                                                       tf.train.NanTensorHook(avg_total_loss),logHook], config=config,
                                                save_summaries_steps=None, save_summaries_secs=None) as mon_sess:
                 while not mon_sess.should_stop():
-                    mon_sess.run(train_op)
+                    # Train, Record stats and save summaries
                     if global_step % flags.save_frequency == 0:
-                        mon_sess.run(train_op, options= run_options, run_metadata=run_metadata)
+                        _, sum_merged = mon_sess.run([train_op, summary_merged], options= run_options, run_metadata=run_metadata)
                         summary_writer = tf.summary.FileWriter(flags.train_dir)
                         summary_writer.add_run_metadata(run_metadata, 'step %s' % format(global_step),
                                                         global_step=global_step)
-                        summary_writer.add_summary(summary_merged, global_step=global_step)
+                        summary_writer.add_summary(sum_merged, global_step=global_step)
+                    else:
+                        # Just train
+                        mon_sess.run(train_op)
 
                 summary_writer.close()
 
