d4f7dc900439b7e66eb8657ff4e608f506d0de48
syz <syz@ornl.gov>
2017-11-15 11:37:04 -0500

    Automated generation of (simple) ResNet-18 and ResNet-34.

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -59,6 +59,75 @@ linear_output = OrderedDict({'type': 'linear_output','weights': 3,'bias': 3,'reg
 layer_params_list = [conv_1]*2 +[pool] + [conv_2]*2 + [pool] + [conv_3]*4 + [pool] + [conv_4]*4 + [pool] + [conv_4]*4 + [pool] + [fully_connected_1]*2 + [fully_connected_2] + [linear_output]
 
 
+# ###########
+# ResNet-18 #
+# ###########
+
+
+def modify_layer(standard, new_parms):
+    modified = standard.copy()
+    modified.update(new_parms)
+    return modified
+
+
+def simple_res_block(conv_layer):
+    return OrderedDict({'type': 'residual', 'conv1': conv_layer, 'conv2': conv_layer})
+
+
+def build_network(sequence):
+    names = list()
+    parms = list()
+    for batch in sequence:
+        layer_name, layer, reps = batch
+        if reps > 1:
+            for index in range(1, reps + 1):
+                names.append(layer_name + '_' + str(index))
+                parms.append(layer)
+        else:
+            names.append(layer_name)
+            parms.append(layer)
+    return names, parms
+
+std_conv = OrderedDict({'type': 'convolutional', 'stride': [1, 1], 'kernel': [3, 3], 'features': 64,
+                        'activation': 'relu', 'padding': 'SAME', 'batch_norm': True})
+conv_0 = modify_layer(std_conv, {'stride': [2, 2], 'kernel': [7, 7]})
+pool_0 = OrderedDict({'type': 'pooling', 'stride': [2, 2], 'kernel': [3, 3], 'pool_type': 'max', 'padding': 'SAME'})
+max_pool_2 = OrderedDict({'type': 'pooling', 'stride': [2, 2], 'kernel': [2, 2], 'pool_type': 'max', 'padding': 'SAME'})
+res_1 = simple_res_block(modify_layer(std_conv, {'features': 64}))
+res_2 = simple_res_block(modify_layer(std_conv, {'features': 128}))
+res_3 = simple_res_block(modify_layer(std_conv, {'features': 256}))
+res_4 = simple_res_block(modify_layer(std_conv, {'features': 512}))
+avg_pool_7 = OrderedDict({'type': 'pooling', 'stride': [7, 7], 'kernel': [7, 7], 'pool_type': 'avg', 'padding': 'SAME'})
+fully_connected = OrderedDict({'type': 'fully_connected', 'weights': 1000, 'bias': 1000, 'activation': 'relu',
+                               'regularize': True})
+linear_output = OrderedDict({'type': 'linear_output', 'weights': 27, 'bias': 27, 'regularize': False})
+
+sequence = [('conv0', conv_0, 1), ('pool0', pool_0, 1),
+            ('res1', res_1, 2), ('pool1', max_pool_2, 1),
+            ('res2', res_2, 2), ('pool2', max_pool_2, 1),
+            ('res3', res_3, 2), ('pool3', max_pool_2, 1),
+            ('res4', res_4, 2), ('pool4', avg_pool_7, 1),
+            ('fc', fully_connected, 1), ('linear_output', linear_output, 1)]
+
+resnet_18_names, resnet_18_parms = build_network(sequence)
+io_utils.write_json_network_config('network_ResNet_18.json', resnet_18_names, resnet_18_parms)
+
+
+# ###########
+# ResNet-34 #
+# ###########
+
+sequence = [('conv0', conv_0, 1), ('pool0', pool_0, 1),
+            ('res1', res_1, 3), ('pool1', max_pool_2, 1),
+            ('res2', res_2, 4), ('pool2', max_pool_2, 1),
+            ('res3', res_3, 6), ('pool3', max_pool_2, 1),
+            ('res4', res_4, 3), ('pool4', avg_pool_7, 1),
+            ('fc', fully_connected, 1), ('linear_output', linear_output, 1)]
+
+resnet_34_names, resnet_34_parms = build_network(sequence)
+io_utils.write_json_network_config('network_ResNet_34.json', resnet_34_names, resnet_34_parms)
+
+
 #################################
 # templates for hyper-parameters #
 #################################
