8b1b1f79f72098557a6527bb97f2145c321b7d9a
syz <syz@ornl.gov>
2018-02-13 16:46:08 -0500

    Added ResNet101 and 152

--- scripts/train_vgg_hvd_json.sh
+++ scripts/train_vgg_hvd_json.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 gpus=2
 script=stemdl_regress_horovod_json_input.py
 echo mpirun --allow-run-as-root -np $gpus python $script
 mpirun --allow-run-as-root -np $gpus python $script
+rm stemdl_checkpoint/ -r
