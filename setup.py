c80e26945559f5890e1c4c93e79f8d5344921291
Nouamane Laanait <nlaanait@login2.summit.olcf.ornl.gov>
2019-01-18 11:06:19 -0500

    update .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -6,32 +6,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
-requirements = ['numpy>=1.13.0', 'tensorflow-gpu>=1.2']
+requirements = ['numpy>=1.13.0', 'tensorflow>=1.2']
 
 setup(
     name='stemdl',
-    version='0.01',
+    version='0.1',
     description='',
     long_description=long_description,
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        ],
-    keywords='scientific microscopy data analysis',
-    packages=find_packages(exclude='tests'),
-    url='https://code-int.ornl.gov/AtomAI',
+    url='https://code.ornl.gov/disMultiABM',
     license='MIT',
     author='N. Laanait',
     author_email='laanaitn@ornl.gov',
@@ -43,27 +25,6 @@ setup(
     tests_require='Nose',
     dependency='',
     dependency_links=[''],
-    include_package_data=True,
+    include_package_data=True
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
 )
