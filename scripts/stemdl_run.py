3dba906cb0b9d63172812df7b5ccd3479b1be4b5
Laanait, Nouamane <laanaitn@ornl.gov>
2019-03-22 16:38:40 -0400

    adding mpi4py + hvd control flow

--- scripts/stemdl_run.py
+++ scripts/stemdl_run.py
@@ -25,6 +25,11 @@ from stemdl import io_utils
 from hspace import small_objective
 from hspace import hyperspace_launcher
 
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+comm_size = comm.Get_size()
+comm_rank = comm.Get_rank()
+
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 def add_bool_argument(cmdline, shortname, longname=None, default=False, help=None):
@@ -47,7 +52,7 @@ def main():
     np.random.seed( 4321 )
 
     # initiate horovod
-    hvd.init()
+    hvd.init(range(1,comm_size))
 
     cmdline = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # Basic options
@@ -211,7 +216,7 @@ def main():
        hyper_params[ 'batch_norm' ][ 'decay' ] = FLAGS.bn_decay
     
     # print relevant params passed to training 
-    if hvd.rank( ) == 0 :
+    if comm_rank == 0 :
        if os.path.isfile( 'cmd.log' ) :
           cmd = open( "cmd.log", "r" )
           cmdline = cmd.readline( )
